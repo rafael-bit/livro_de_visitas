b2bfc140fb0640c4ab5c518189eaebd76830f666
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-10-14 17:50:37 -0300

    add define_timezone

--- config.php
+++ config.php
@@ -8,10 +8,14 @@ define('BASEPATH', 'http://localhost/livro_de_visitas');
 define('VIEW', './app/view');
 define('TITLE', 'Livro de Visitas -SECITEC');
 
+// define('TIMEZONE', 'America/Sao_Paulo'); 
+
+// date_default_timezone_set(TIMEZONE);  
+
 define('BD', [
   'server' => '',
   'port' => '',
   'database' => '',
   'user' => '',
-  'password' => ''
+  'password' => '' 
 ]);
