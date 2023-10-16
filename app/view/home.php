6d4a48b0a6b212f5fa5ab76abcbdae5da71d8b85
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2023-10-16 14:18:14 -0300

    rating

--- app/view/home.php
+++ app/view/home.php
@@ -8,16 +8,16 @@
 
     <label for="rating">Nota do Evento:</label>
     <div class="rating">
-      <input type="radio" id="star1" class="star" name="rating" value="1">
-      <label for="star1">☆</label>
-      <input type="radio" id="star2" class="star" name="rating" value="2">
-      <label for="star2">☆</label>
-      <input type="radio" id="star3" class="star" name="rating" value="3">
-      <label for="star3">☆</label>
-      <input type="radio" id="star4" class="star" name="rating" value="4">
-      <label for="star4">☆</label>
       <input type="radio" id="star5" class="star" name="rating" value="5">
       <label for="star5">☆</label>
+      <input type="radio" id="star4" class="star" name="rating" value="4">
+      <label for="star4">☆</label>
+      <input type="radio" id="star3" class="star" name="rating" value="3">
+      <label for="star3">☆</label>
+      <input type="radio" id="star2" class="star" name="rating" value="2">
+      <label for="star2">☆</label>
+      <input type="radio" id="star1" class="star" name="rating" value="1">
+      <label for="star1">☆</label>
 
     </div>
 
